0bfd9c22c6a2309f04a8cb5207a1217027f43293
Michael Herrmann <michael@herrmann.io>
2022-01-13 10:28:28 -0300

    Fix compilatior error

--- chromium_src/courgette/third_party/bsdiff/paged_array.h
+++ chromium_src/courgette/third_party/bsdiff/paged_array.h
@@ -6,14 +6,18 @@
 #ifndef BRAVE_CHROMIUM_SRC_COURGETTE_THIRD_PARTY_BSDIFF_PAGED_ARRAY_H_
 #define BRAVE_CHROMIUM_SRC_COURGETTE_THIRD_PARTY_BSDIFF_PAGED_ARRAY_H_
 
+namespace courgette {
+
 // Chromium's courgette.exe runs into memory allocation errors when called on
 // Brave's binaries. Reducing the log page size from 18 (=1 MB) to 14
 // (=1/16th MB) fixes this. This makes it seem likely that the errors are
 // caused by address space fragmentation. Unfortunately, we have not yet been
 // able to find the root cause for this. So we use this workaround.
-#define kPagedArrayDefaultPageLogSize \
-  kPagedArrayDefaultPageLogSize = 14; \
-  constexpr int kPagedArrayDefaultPageLogSize_Unused
+constexpr int kPagedArrayDefaultPageLogSize = 14;
+
+}
+
+#define kPagedArrayDefaultPageLogSize kPagedArrayDefaultPageLogSize_Unused
 
 #include "src/courgette/third_party/bsdiff/paged_array.h"
 
