fe44d7cedd8ba2dde393d447a095d78f5fee8816
Nick von Pentz <nickvonpentz@protonmail.com>
2022-03-23 17:30:32 -0400

    Add metadata support for ERC721 tokens

--- components/brave_wallet/browser/eth_response_parser.h
+++ components/brave_wallet/browser/eth_response_parser.h
@@ -43,6 +43,11 @@ bool ParseUnstoppableDomainsProxyReaderGetMany(
 bool ParseUnstoppableDomainsProxyReaderGet(const std::string& json,
                                            std::string* value);
 
+bool ParseERC721TokenUri(const std::string& json, GURL* url);
+
+// Get the JSON included in a data URI with a mime type application/json
+bool ParseDataURIAndExtractJSON(const GURL url, std::string* json);
+
 }  // namespace eth
 
 }  // namespace brave_wallet
