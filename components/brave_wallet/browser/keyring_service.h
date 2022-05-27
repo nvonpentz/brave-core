c3d94e75fe6ee8a7246a275f7839a08746ff9087
Nick von Pentz <nickvonpentz@protonmail.com>
2022-05-27 11:35:45 -0400

    AddAccounts

--- components/brave_wallet/browser/keyring_service.h
+++ components/brave_wallet/browser/keyring_service.h
@@ -289,6 +289,7 @@ class KeyringService : public KeyedService, public mojom::KeyringService {
   FRIEND_TEST_ALL_PREFIXES(KeyringServiceUnitTest, ImportFilecoinAccounts);
   FRIEND_TEST_ALL_PREFIXES(KeyringServiceUnitTest, PreCreateEncryptors);
   FRIEND_TEST_ALL_PREFIXES(KeyringServiceUnitTest, HardwareAccounts);
+  FRIEND_TEST_ALL_PREFIXES(KeyringServiceUnitTest, AccountsAdded);
 
   FRIEND_TEST_ALL_PREFIXES(KeyringServiceAccountDiscoveryUnitTest,
                            AccountDiscovery);
@@ -376,6 +377,9 @@ class KeyringService : public KeyedService, public mojom::KeyringService {
   void MaybeMigratePBKDF2Iterations(const std::string& password);
 
   void NotifyAccountsChanged();
+  void NotifyAccountsAdded(
+      const std::vector<mojom::AccountInfoPtr> account_infos);
+  // void NotifyAccountsRemoved();
   void StopAutoLockTimer();
   void ResetAutoLockTimer();
   void OnAutoLockPreferenceChanged();
