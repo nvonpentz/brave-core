53fa3dac9e5175fe85570797a8df759489c11836
nvonpentz <12549658+nvonpentz@users.noreply.github.com>
2022-11-01 14:10:35 -0400

    Fix brave_browser_test compilation
    Add missing OnDiscoveredAssetsCompleted implementation in
    TestJsonRpcServiceObserver in brave_wallet_ethereum_chain_browsertest.cc.

--- browser/brave_wallet/brave_wallet_ethereum_chain_browsertest.cc
+++ browser/brave_wallet/brave_wallet_ethereum_chain_browsertest.cc
@@ -135,6 +135,10 @@ class TestJsonRpcServiceObserver
   void OnIsEip1559Changed(const std::string& chain_id,
                           bool is_eip1559) override {}
 
+  void OnDiscoveredAssetsCompleted(const std::string& chain_id,
+                                   brave_wallet::mojom::ProviderError error,
+                                   const std::string& error_message) override {}
+
   bool chain_changed_called() {
     base::RunLoop().RunUntilIdle();
     return chain_changed_called_;
